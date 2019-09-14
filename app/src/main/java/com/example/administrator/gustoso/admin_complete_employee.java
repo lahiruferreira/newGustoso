5247c2983a190095d1c6e6faf7f87ee5b71575b3
Minindu Sudantha <minindu.iul@gmail.com>
2019-09-14 11:43:25 +0530

    Minindu design2

---
+++ app/src/main/java/com/example/administrator/gustoso/admin_complete_employee.java
@@ -0,0 +1,13 @@
+package com.example.administrator.gustoso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class admin_complete_employee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_complete_employee);
+    }
+}

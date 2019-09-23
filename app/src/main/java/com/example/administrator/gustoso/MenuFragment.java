b6459f6afe6952a5c31ca544680729d7370f312e
lahiruferreira <lahriuferreira444@gmail.com>
2019-09-23 20:28:28 +0530

    OK

--- app/src/main/java/com/example/administrator/gustoso/MenuFragment.java
+++ app/src/main/java/com/example/administrator/gustoso/MenuFragment.java
@@ -15,33 +15,29 @@ import android.widget.Button;
 
 public class MenuFragment extends Fragment {
 
-    Button btn_btn1;
-    Button btn_btn2;
+    Button btn;
+    Button btn2;
+   /* Button btn_btn2;
     Button btn_btn3;
-    Button btn_btn4;
+    Button btn_btn4;*/
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_menu,container,false);
 
-        btn_btn1 =(Button) view.findViewById(R.id.FoodDetails_btn1);
-        btn_btn2 =(Button) view.findViewById(R.id.FoodDetails_btn2);
-        btn_btn3 =(Button) view.findViewById(R.id.FoodDetails_btn3);
-        btn_btn4 =(Button) view.findViewById(R.id.FoodDetails_btn4);
+        Button btn = (Button)view.findViewById(R.id.btnOrder);
 
-        return view;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        btn_btn1.setOnClickListener(new View.OnClickListener() {
+       btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), FriedRiceList.class);
+                Intent intent = new Intent(getActivity(),Orderfood.class);
                 startActivity(intent);
             }
         });
 
+
+        return view;
     }
 }
+
+
